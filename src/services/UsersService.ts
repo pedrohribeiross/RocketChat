58b3317d79dce74c0de664b95f9a9a3892475256
Pedro Henrique <pedro_henrique.rss@hotmail.com>
2021-04-21 16:25:37 -0300

    Third day class.

---
+++ src/services/UsersService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UsersService {
+	private usersRepository: Repository<User>;
+	constructor() {
+		this.usersRepository = getCustomRepository(UsersRepository);
+	}
+
+	async create(email: string) {
+		// Verificar se o usuario existe.
+		const userExists = await this.usersRepository.findOne({
+			email,
+		});
+
+		// Se existir, retornar usuario.
+		if (userExists) {
+			return userExists;
+		}
+
+		const user = this.usersRepository.create({
+			email,
+		});
+
+		await this.usersRepository.save(user);
+
+		// Se não existir, salvar no BD.
+		return user;
+	}
+}
+
+export { UsersService };
